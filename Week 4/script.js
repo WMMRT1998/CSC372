7dbfbecb1d71f292ddf14ff383b35fa7a3f5bed2
Russell <181926470+WMMRT1998@users.noreply.github.com>
2026-09-10 16:34:49 -0400

    js

---
+++ Week 4/script.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+const person = {
+    name: "John Doe",
+    age: 30,
+    city: "New York"};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
